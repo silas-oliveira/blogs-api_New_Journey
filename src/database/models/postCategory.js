7bdccadab5d8dd7c6d4f0c80d290d72283884b16
Silas <silas.oliveira8@gmail.com>
2022-07-19 18:11:26 -0300

    feat: req 11 completed

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,34 @@
+'use strict'
+/**
+ * @param {import('sequelize').Sequelize } sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+const createPostCategoriesModel = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory",
+    {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+    },
+    { timestamps: false }
+  );
+  
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    })
+  }
+
+  return PostCategory;
+};
+
+module.exports = createPostCategoriesModel;
